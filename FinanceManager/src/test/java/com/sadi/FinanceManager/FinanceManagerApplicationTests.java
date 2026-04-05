7d6e474e334338c391d9d5cf093f3785e7bb3b76
Saditha Fernando <sadithafernandods2002@gmail.com>
2026-04-05 10:01:20 +0530

    Spring-Initialization

---
+++ FinanceManager/src/test/java/com/sadi/FinanceManager/FinanceManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sadi.FinanceManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
