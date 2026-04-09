ad7db7fa6b93d050b5f298065712ce9983e5e83f
Saditha Fernando <sadithafernandods2002@gmail.com>
2026-04-09 10:49:42 +0530

    9 Api structure

---
+++ Finance_Manager_FrontEnd/src/assets/assets.js
@@ -0,0 +1,8 @@
+import logo from "./logo.png";
+import loginBackground from "./login-background.png";
+
+export const assets = {
+    logo,
+    loginBackground,
+}
+
---
+++ Finance_Manager_FrontEnd/src/context/AppContext.jsx
@@ -0,0 +1,20 @@
+import {createContext, useState} from "react";
+
+export const AppContext = createContext();
+
+export const AppContextProvider = ({children}) => {
+
+    const [user, setUser] = useState(null);
+
+
+
+    const contextValue = {
+        user,
+    }
+
+    return (
+        <AppContext.Provider value={contextValue}>
+            {children}
+        </AppContext.Provider>
+    )
+}
