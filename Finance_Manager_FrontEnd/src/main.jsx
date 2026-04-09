68944154d707219dc50e93bc95a2aeba4d266613
Saditha Fernando <sadithafernandods2002@gmail.com>
2026-04-09 11:07:10 +0530

    10 need for collaboration

---
+++ Finance_Manager_FrontEnd/src/main.jsx
@@ -0,0 +1,14 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import {AppContextProvider} from "./context/AppContext.jsx";
+
+createRoot(document.getElementById('root')).render(
+    <AppContextProvider>
+  {/*<StrictMode>*/}
+  {/*  <App />*/}
+  {/*</StrictMode>,*/}
+        <App />
+    </AppContextProvider>
+)
