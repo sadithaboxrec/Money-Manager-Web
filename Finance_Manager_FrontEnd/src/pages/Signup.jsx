528de2f08b5513b45c3aac2fce573d04ab166dbe
Saditha Fernando <sadithafernandods2002@gmail.com>
2026-04-09 11:08:30 +0530

    10.1 need for collaboration

--- Finance_Manager_FrontEnd/src/pages/Signup.jsx
+++ Finance_Manager_FrontEnd/src/pages/Signup.jsx
@@ -1,4 +1,15 @@
+import {useState} from "react";
+import {useNavigate} from "react-router-dom";
+
 const Signup = () => {
+
+    const [fullName, setFullName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState(null);
+
+
+    const navigate = useNavigate();
 
     return(
         <div>Signup</div>
