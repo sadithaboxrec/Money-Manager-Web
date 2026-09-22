08d02af12f38360912a11a720feccc9a03ecc8e8
Saditha Fernando <sadithafernandods2002@gmail.com>
2026-09-23 00:32:05 +0530

    Add CI for backend and frontend

--- FinanceManager/src/test/java/com/sadi/FinanceManager/FinanceManagerApplicationTests.java
+++ FinanceManager/src/test/java/com/sadi/FinanceManager/FinanceManagerApplicationTests.java
@@ -2,12 +2,16 @@ package com.sadi.FinanceManager;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mail.javamail.JavaMailSender;
 
 @SpringBootTest
 class FinanceManagerApplicationTests {
 
+	@MockBean
+	private JavaMailSender javaMailSender;
+
 	@Test
 	void contextLoads() {
 	}
-
 }
